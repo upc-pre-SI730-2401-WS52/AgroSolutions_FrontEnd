b3f733dcea093eceb7a0767d47396a91d6712eea
GustavoPoma <157068395+GustavoPoma@users.noreply.github.com>
2024-05-27 13:44:47 -0500

    Add files via upload

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded-lg">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: String
+  }
+}
+</script>
+
+<style scoped>
+button {
+  transition: background-color 0.3s;
+}
+</style>
