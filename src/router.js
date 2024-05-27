82577f31f410e59e6e968ee58f4194be72156af5
GustavoPoma <157068395+GustavoPoma@users.noreply.github.com>
2024-05-27 13:48:08 -0500

    Update router.js

--- src/router.js
+++ src/router.js
@@ -7,12 +7,18 @@ import TheFooter from '@/components/the-footer.component.vue'
 
 import { createRouter, createWebHistory } from 'vue-router'
 
+import TheEmployeesComponent from '@/components/TheEmployeesComponent.vue';
+import Equipment from '@/components/equipment.vue';
+
+
 const routes = [
     { path: '/', component: TheMain },
     { path: '/login', component: TheLogin },
     { path: '/register', component: TheRegister },
     { path: '/home', component: TheHome },
     { path: '/users', component: TheLogin },
+     { path: '/employees', name: 'employees', component: TheEmployeesComponent },
+    { path: '/equipment', name: 'equipment', component: Equipment }
     //{ path: '/:pathMatch(.*)*', component: NotFoundComponent }
 ]
 
