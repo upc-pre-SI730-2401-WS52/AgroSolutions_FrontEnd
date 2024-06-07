ca16e044f44fb8c2864bb1b676a72ae0b175a814
GustavoPoma <157068395+GustavoPoma@users.noreply.github.com>
2024-06-07 13:48:27 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: 'https://GustavoPoma.github.io/upc-pre-SI730-2401-WS52/FrontEnd_AgroSolutions/',
+  plugins: [vue(),] ,
+ base: 'https://GustavoPoma.github.io/upc-pre-SI730-2401-WS52/FrontEnd_AgroSolutions/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
